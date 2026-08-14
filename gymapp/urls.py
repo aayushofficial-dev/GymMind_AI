861069e7d3ecb02b63b04afc6db68f88d4089a6a
Buildwithaayush <iaayush.dev@gmail.com>
2026-08-14 21:33:59 +0545

    changes in views.py and added urls

--- gymapp/urls.py
+++ gymapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', home, name='home'), #include gymapp URLs
+    path('', home, name='home'), #include gymapp URLs
+    path('about/', about, name='about'),
+    path('admin_login', admin_login_view, name='admin_login'),
+    path('admin-dashboard/', admin_dashboard_view, name='admin_dashboard'),
+    path('logout/', logout_view, name='logout')
+
+    
 ]
--- gymapp/views.py
+++ gymapp/views.py
@@ -29,3 +29,43 @@ def home(request):
             message.error(request, 'Please fill in all the fields before submitting the form')
 
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+from django.contrib.auth import authenticate, login, logout
+
+def admin_login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None and getattr(user, 'role', None) == 'ADMIN': # check if the user is an admin/staff
+            login(request, user) # log the user in using Django's built-in login function
+            messages.success(request, 'Logged in successfully!.')
+            return redirect('admin_dashboard') # Redirect to the admin dashboard
+
+        else:
+            messages.error(request, 'Invalid credentials or not an admin')
+    return render(request, 'admin_login.html')
+
+def admin_required(view_func):
+    '''
+    Decorator to ensure that the user is an admin before accessing certain views.
+    '''
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated or getattr(request.user, 'role', None) != 'ADMIN':
+            messages.error(request, 'You must be logged in as an admin to access this page.')
+            return redirect('admin_login') # Redirect to the admin login page
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+@admin_required
+def admin_dashboard_view(request):
+    return render(request, 'admin_dashboard.html')
+
+def logout_view(request):
+    logout(request)  # log the user out using Django's built-in logout function
+    messages.success(request, 'Logged out successfully!')
+    return redirect('home') # Redirect to the home page after logout
+
